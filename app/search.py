fe16d09333d500dc1b95910966ad05203070720e
McKenna <mckenna.m.mcmurray@gmail.com>
2021-12-16 18:30:49 -0600

    Add 2 srch func -> iterate thr. user inpt & JOIN ON root string

--- app/search.py
+++ app/search.py
@@ -9,26 +9,37 @@ from app.db import get_db
 bp = Blueprint("search", __name__)
 @bp.route("/search", methods=('GET', 'POST'))
 def search():
+	filters = ['Title', 'Author', 'Cooktime']
 	if request.method == "GET":
-		return render_template('search/searchbar.html')
+		return render_template('search/searchbar.html', filters=filters)
 
 	if request.method == "POST" :
-		recipe_name = request.form["title"]
+		select_1 = request.form.get('filter')
+		select_2 = request.form.get('filter_2')
+		input_1 = request.form["form_1"]
+		input_2 = request.form.get("form_2")
+
+
 		error = None
 
 		if error is not None:
 			flash(error)
 		else:
 			db = get_db()
-			r = title_request(recipe_name)
-		return render_template("cookbook/index.html", recipes=r)
+			r = search_request(select_1, select_2, input_1, input_2)
+		#return render_template("cookbook/index.html", recipes=r)
+		return(str(input_1 + select +  input_2 + select_2))
 
 
-
-def title_request(recipe_name):
+def search_request():
 	db = get_db()
-	query = """SELECT recipe.id , recipe_name, instructions, create_date, author_id, username FROM recipe JOIN user ON recipe.author_id = user.id WHERE recipe_name = ? ORDER BY create_date DESC"""
-	recipe = db.execute(query, (recipe_name,)).fetchall()
+	root_query =  f"""SELECT recipe.id , recipe_name, instructions, create_date, author_id, username FROM recipe"""
+
+	if select_1 != "":
+		if select_1 == "Title":
+			
+
+	recipe = db.execute(query).fetchall()
 
 	if recipe is None:
 		abort(404, f"Recipe name {recipe_name} doesn't exist.") # Not Found
